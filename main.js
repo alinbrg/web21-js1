68031f556c5799d6db613a15eddf08c8ddb265d3
Alina Bregvadze <alina.bregvadze@gmail.com>
2023-07-12 22:21:13 +0400

    js variables

---
+++ main.js
@@ -0,0 +1,78 @@
+console.log("hello from js file");
+
+var nameWithVar = 234;
+let nameWithLet = "alina";
+const nameWithConst = "ALINA";
+
+nameWithLet = 24;
+// nameWithoutKeyword = "Nika";
+// console.log(nameWithVar, nameWithLet, nameWithConst);
+
+const num1 = 24;
+const num2 = 24.5;
+
+// console.log("typeof num1 ", typeof num1);
+
+const str1 = "text1's ";
+const str2 = 'text2  \'"new text" ';
+const str3 = `text3  
+
+text new line
+`;
+
+// console.log(typeof str1);
+
+const isPasswordMoreThen8 = false;
+
+// console.log(typeof isPasswordMoreThen8);
+
+const undefined1 = 10;
+let undefined2;
+
+// console.log(typeof undefined1);
+undefined2 = 10;
+// console.log(undefined1, undefined2);
+
+const myNull = null;
+// console.log(typeof myNull);
+
+undefined2 = 20;
+
+undefined2 = undefined;
+
+const num3 = 10;
+const num4 = 20;
+
+const add = num3 + num4;
+console.log(add);
+const substraction = num3 - num4;
+console.log(substraction);
+const multiplication = num3 * num4;
+console.log(multiplication);
+const division = num3 / num4;
+console.log(division);
+
+const power = num3 ** 3;
+console.log(power);
+
+const modulo = num3 % 7;
+
+/*
+console.log(modulo);
+
+*/
+
+// NaN not a number
+
+// console.log("text" + " another text");
+// console.log("text" + 5 + " another text");
+// console.log(16 + 4 + "text");
+// console.log(16 + "text" + 4);
+// console.log(16 + "4" + 4);
+
+const userName = "Alina"; //comment
+const userLastName = "Bregvadze";
+
+console.log("hello, " + userName + " " + userLastName + "!");
+
+console.log(`hello, ${userName} ${userLastName}!`);
